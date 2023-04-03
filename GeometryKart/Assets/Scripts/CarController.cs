fc967836b4f704863e13dc8cf8004177c7b17e09
Jan Ruppert <104840213+henru101@users.noreply.github.com>
2023-04-03 14:42:19 +0200

    added basic multiplayer
    added networktesting ui
    modified movement for multiplayer

--- GeometryKart/Assets/Scripts/CarController.cs
+++ GeometryKart/Assets/Scripts/CarController.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class CarController : MonoBehaviour
+public class CarController : NetworkBehaviour
 {
     
     //input
@@ -45,21 +46,64 @@ public class CarController : MonoBehaviour
 
     private void Update()
     {
+        if (!IsOwner)
+        {
+            return;
+        }
+
+        HandleMovementServerAuth();
+        //HandleMovement();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!IsOwner)
+        {
+            return;
+        }
+        FixedHandleMovementServerRpc();
+    }
+
+    private void HandleMovementServerAuth()
+    {
+        HandleMovementServerRpc(accelerateAction.ReadValue<float>(), steerAction.ReadValue<float>());
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void HandleMovementServerRpc(float accelerateValue, float steerValue)
+    {
+        transform.position = sphere.transform.position - new Vector3(0, sphere.GetComponent<SphereCollider>().radius, 0);
+        
+        currentSpeed = Mathf.SmoothStep(currentSpeed, acceleration * accelerateValue, Time.deltaTime * 12f);
+        
+        currentRotate = Mathf.Lerp(currentRotate, steeringSensitivity *  steerValue, Time.deltaTime * 4f);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void FixedHandleMovementServerRpc()
+    {
+        //acceleration
+        sphere.GetComponent<Rigidbody>().AddForce(-transform.forward * currentSpeed, ForceMode.Acceleration);
+        
+        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y + currentRotate, 0), Time.fixedDeltaTime * 5f);
+    }
+
+
+    private void HandleMovement()
+    {
         transform.position = sphere.transform.position - new Vector3(0, sphere.GetComponent<SphereCollider>().radius, 0);
         
         currentSpeed = Mathf.SmoothStep(currentSpeed, acceleration * accelerateAction.ReadValue<float>(), Time.deltaTime * 12f);
         
         currentRotate = Mathf.Lerp(currentRotate, steeringSensitivity *  steerAction.ReadValue<float>(), Time.deltaTime * 4f);
-        Debug.Log(currentSpeed);
     }
 
-    private void FixedUpdate()
+    private void FixedHandleMovement()
     {
         //acceleration
         sphere.GetComponent<Rigidbody>().AddForce(-transform.forward * currentSpeed, ForceMode.Acceleration);
         
-        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y + currentRotate, 0), Time.deltaTime * 5f);
+        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y + currentRotate, 0), Time.fixedDeltaTime * 5f);
     }
-    
     
 }
---
+++ GeometryKart/Assets/Scripts/TestingNetworkUI.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestingNetworkUI : MonoBehaviour
+{
+    [SerializeField] private Button startHostButton;
+    [SerializeField] private Button startClientButton;
+
+
+    private void Awake()
+    {
+        startHostButton.onClick.AddListener((() =>
+                {
+                    Debug.Log("Started Host!");
+                    NetworkManager.Singleton.StartHost();
+                    Hide();
+                }
+            ));
+        
+        startClientButton.onClick.AddListener((() =>
+                {
+                    Debug.Log("Started Client!");
+                    NetworkManager.Singleton.StartClient();
+                    Hide();
+                }
+            ));
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
